810ca0152e986e64e02c17172dcbc51fef4b9ed1
hooiyan <hoecoding@gmail.com>
2022-03-30 15:38:36 +0800

    Add search button

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,7 @@
 import { useRecoilState } from 'recoil'
 import { queryAtom } from '../recoil/atoms'
 import IconSearch from './icons/IconSearch'
+import SearchButton from './SearchButton'
 
 export default function SearchBar({
   handleSubmit,
@@ -12,12 +13,13 @@ export default function SearchBar({
     <form onSubmit={handleSubmit} className="flex pb-6 lg:mt-9 md:pb-10">
       <IconSearch className="h-6 w-6 md:h-8 md:w-8" />
       <input
-        className="bg-app-dark-blue caret-app-red w-full rounded-none pb-[8px] ml-4 border-b border-app-dark-blue text-base font-light placeholder:text-app-placeholder placeholder:text-base focus:outline-none focus:border-b focus:border-app-greyish-blue md:heading-md md:placeholder:heading-md"
+        className="bg-app-dark-blue caret-app-red w-full rounded-none pb-[8px] mx-4 border-b border-app-dark-blue text-base font-light placeholder:text-app-placeholder placeholder:text-base focus:outline-none focus:border-b focus:border-app-greyish-blue md:heading-md md:placeholder:heading-md"
         type="text"
         placeholder={placeholder}
         onChange={e => setQuery(e.target.value)}
         value={query}
       />
+      <SearchButton />
     </form>
   )
 }
---
+++ components/SearchButton.js
@@ -0,0 +1,9 @@
+export default function SearchButton() {
+  return (
+    <button
+      type="submit"
+      className="bg-app-semi-dark-blue text-app-pure-white text-xs py-2 px-3 rounded-md flex items-center justify-center hover:bg-app-greyish-blue">
+      Search
+    </button>
+  )
+}
