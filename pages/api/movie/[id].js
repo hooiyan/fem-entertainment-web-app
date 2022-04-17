125db7ba5635715fa0eed4481612aec1d254fde1
hoeho <hoecoding@gmail.com>
2022-04-17 08:31:53 +0800

    Fix assets issue

--- pages/api/movie/[id].js
+++ pages/api/movie/[id].js
@@ -1,4 +1,4 @@
-import { getMovieDetail, getMovieCasts } from '../../../lib/tmdb'
+import { getMovieCasts, getMovieDetail } from '../../../lib/tmdb'
 
 export default async function handler(req, res) {
   const { id } = req.query
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     const data2 = await response2.json()
     res.status(200).json({
       detail: data,
-      credits: data2
+      credits: data2,
     })
   } catch (err) {
     res.status(500).json({ error: err.message })
