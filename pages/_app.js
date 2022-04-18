6082c676715f316a8cd83674f290a9984bfb7560
hoeho <hoecoding@gmail.com>
2022-04-18 08:40:33 +0800

    Add progress bar at the top using nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import { RecoilRoot } from 'recoil'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import '../styles/nprogress.css'
+
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
