a19afdc5f378f3343805c84e7945f2339af15112
hooiyan <hoecoding@gmail.com>
2022-04-27 10:31:49 +0800

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
